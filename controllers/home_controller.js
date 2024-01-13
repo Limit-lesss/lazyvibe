5848072c2db29fa626c33bd59c56af0053e7b5c8
Pranav Kumar <engineersempire0204@gmail.com>
2024-01-14 00:37:11 +0530

    sign in manually

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,6 @@
 const User = require("../models/user");
 
 module.exports.home = (req, res) => {
-  console.log(req.cookies);
-  res.cookie("genius", 428);
   User.find({}).then((auth_list) => {
     return res.render("home", {
       title: "Lazyvibe",
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,10 +16,10 @@ module.exports.createUser = (req, res) => {
           password: req.body.password,
           name: req.body.name,
         })
-          .then((user) => res.redirect('/users/sign-in'))
+          .then((user) => res.redirect("/users/sign-in"))
           .catch((err) => console.log("error on creating user " + err));
       } else {
-        res.render("user_sign_up", {
+        return res.render("user_sign_up", {
           title: "Lazyvibe | Sign Up",
           isPasswordNotMatch: false,
           isEmailExist: true,
@@ -27,7 +27,7 @@ module.exports.createUser = (req, res) => {
       }
     })
     .catch((err) => {
-      if (err) console.log("error in finding user in signing up");
+      console.log("error in finding user in signing up", err);
       return;
     });
 };
@@ -41,14 +41,44 @@ module.exports.signUp = (req, res) => {
 };
 //! render sign in page
 module.exports.signIn = (req, res) => {
-  return res.render("user_sign_in", { title: "Lazyvibe | Sign In" });
+  return res.render("user_sign_in", {
+    title: "Lazyvibe | Sign In",
+    isPasswordNotMatch: false,
+  });
 };
 
 module.exports.profile = (req, res) => {
-  return res.end("<h1>User profile</h1>");
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id)
+      .then((user) => {
+        return res.render("profile", { userName: user.name });
+      })
+      .catch((err) => console.log("err: ", err));
+  } else res.redirect("/users/sign-in");
 };
 module.exports.post = (req, res) => {
   return res.end("<h1>User posts</h1>");
 };
 
-module.exports.createSession = (req, res) => {};
+module.exports.createSession = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        if (user.password === req.body.password) {
+          res.cookie("user_id", user.id);
+          return res.render("profile", { userName: user.name });
+        } else {
+          return res.render("user_sign_in", {
+            title: "Lazyvibe | Sign In",
+            isPasswordNotMatch: true,
+          });
+        }
+      } else {
+        return res.redirect("back");
+      }
+    })
+    .catch((err) => {
+      console.log("error in finding user in signing in", err);
+      return;
+    });
+};
